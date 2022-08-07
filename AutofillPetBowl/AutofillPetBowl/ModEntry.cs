8f30329e9b7f3db5e8a65dac62e3dcfaa1f98bc3
maephie <maephie.business@protonmail.com>
2022-08-07 02:24:40 +0200

    Add AutofillPetBowl

---
+++ AutofillPetBowl/AutofillPetBowl/ModEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace AutofillPetBowl
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+        }
+
+        private void GameLoop_DayStarted(object? sender, DayStartedEventArgs e)
+        {
+            Farm farm = Game1.getFarm();
+            farm.petBowlWatered.Set(true);
+        }
+    }
+}
